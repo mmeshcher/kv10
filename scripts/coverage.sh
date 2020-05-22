afe3a3a26bd34f47c94797f3fa3ef74fe38c0293
mmeshcher <keyvrn12012014@icloud.com>
2020-05-22 16:07:29 +0300

    f

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -11,5 +11,3 @@ CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
 cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON 
 cmake --build _builds
-cmake --build _builds --target test
-
